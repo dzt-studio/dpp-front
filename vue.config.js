37a814e4c0d21e56496490d953e5ebb97da08289
tandz <tandengzhao@d1000.cn>
2021-07-12 16:21:09 +0800

    readme

--- vue.config.js
+++ vue.config.js
@@ -30,6 +30,8 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
+    disableHostCheck: true,
+    host: '0.0.0.0',
     port: port,
     open: true,
     overlay: {
